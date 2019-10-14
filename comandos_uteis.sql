e80435362e68d2be1bf3d354a3f5cfffed499917
MateusArthur <40706262+MateusArthur@users.noreply.github.com>
2019-10-14 15:57:54 -0300

    Update comandos_uteis.sql

--- comandos_uteis.sql
+++ comandos_uteis.sql
@@ -10,3 +10,4 @@ explain select distinct p.nome from filme f  join pessoa p on(f.codd = p.codp) j
 
 select nome from pessoa p natural join elenco e intersect select nome from pessoa p join filme f on p.codp = f.codd;
 
+select f.titulo from filme f natural join elenco e group by f.titulo having count(*) = (select max (t.maior) from (select count(*) as maior from elenco group by codf) t);
