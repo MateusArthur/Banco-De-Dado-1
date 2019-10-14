093f79239b608f7306793285d7e34d4daad15db8
MateusArthur <40706262+MateusArthur@users.noreply.github.com>
2019-10-14 14:32:43 -0300

    Update comandos_uteis.sql

--- comandos_uteis.sql
+++ comandos_uteis.sql
@@ -1,5 +1,8 @@
 #Comandos uties sql
 
-select nome from aluno where dtnasc = (select mint(dtnasc) from aluno where codc = 11)
+select nome from aluno where dtnasc = (select mint(dtnasc) from aluno where codc = 11);
 
 select a.nome from (select min(dtnasc) as eldery from aluno where codc = 11) t join aluno a on t.eldery = a.dtnasc where a.code = 11;
+
+select nomep, sum(qtdade) from exp group by 1;
+
